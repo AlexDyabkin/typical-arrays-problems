16b1dab333a16a2f7843001b5d6a69a8f78d51a0
AlexDyabkin <alexendredyabkin@gmail.com>
2020-02-22 14:51:31 +0300

    faet: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===null){return 0;
+  }
+  else {
+  return Math.min.apply(null,array)}
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array.length===0){return 0;}
+  else {
+  return Math.max.apply(null,array);
+}}
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array.length===0){return 0;}
+  else{
+  return Math..apply(null,array);
+}}
